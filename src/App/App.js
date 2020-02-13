2b3540fdcaa4e94e054176984e16fb859b148b7b
groupanimal <groupanimal@posteo.net>
2020-02-13 02:17:52 +0000

    add styled components for all headings fiddle with fonts

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './App.css'
+import { H1 } from '../styled'
+import styled from 'styled-components'
 
-function App() {
-  return <div className='App'></div>
+const App = () => {
+  return (
+    <div className='app'>
+      <H1>DAN GOSS</H1>
+    </div>
+  )
 }
+
+styled(App)`
+  padding: var(--component-padding);
+`
 
 export default App
---
+++ src/styled/headings.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-family: var(--heading);
+  font-weight: 500;
+  font-size: 3em;
+  letter-spacing: 0.1em;
+`
+
+const H2 = styled.h2`
+  font-family: var(--heading);
+  font-weight: 500;
+  font-size: 2em;
+`
+
+const H3 = styled.h3`
+  font-family: var(--heading);
+  font-weight: 500;
+  font-size: 1.6em;
+`
+
+const H4 = styled.h4`
+  font-family: var(--heading);
+  font-weight: 500;
+  font-size: 1.3em;
+`
+
+const H5 = styled.h5`
+  font-family: var(--heading);
+  font-weight: 700;
+  font-size: 1.1em;
+`
+
+const H6 = styled.h6`
+  font-family: var(--heading);
+  font-weight: 700;
+  font-size: 1em;
+`
+
+export { H1, H2, H3, H4, H5, H6 }
